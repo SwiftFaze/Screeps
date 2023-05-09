b71de21f39b370cae9c69cb627ef92bdf352a44c
Rob <RJJW.dev@gmail.com>
2023-05-09 07:51:36 +0200

    Change upgrader amount level 4 to 3

--- src/main/creepComponents.js
+++ src/main/creepComponents.js
@@ -91,7 +91,7 @@ const creepBuilds = {
             body: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, WORK, MOVE, MOVE, MOVE, MOVE]
         }, upgrader: {
             priority: 3,
-            quantity: 5,
+            quantity: 3,
             body: [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
         }, builder: {
             priority: 4,
@@ -109,7 +109,7 @@ const creepBuilds = {
             body: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, WORK, MOVE, MOVE, MOVE, MOVE]
         }, upgrader: {
             priority: 3,
-            quantity: 5,
+            quantity: 3,
             body: [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
         }, builder: {
             priority: 4,
