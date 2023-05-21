5191c1a085e98787c383e2615e6bc7c8f870f937
Rob <RJJW.dev@gmail.com>
2023-05-21 13:23:32 +0200

    Add lvl 6 creeps

--- src/main/creepComponents.js
+++ src/main/creepComponents.js
@@ -124,7 +124,7 @@ const creepBuilds = {
         }, transporter: {
             priority: 2,
             quantity: 1,
-            body: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, WORK, MOVE, MOVE, MOVE, MOVE]
+            body: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
         }, upgrader: {
             priority: 3,
             quantity: 4,
