798f78d872f07e2aa08eea0a476d17e5fb89d850
Rob <RJJW.dev@gmail.com>
2023-05-29 17:38:44 +0200

    +1  upgrader for level 6

--- src/main/creepComponents.js
+++ src/main/creepComponents.js
@@ -127,7 +127,7 @@ const creepBuilds = {
             body: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
         }, upgrader: {
             priority: 3,
-            quantity: 4,
+            quantity: 5,
             body: [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
         }, builder: {
             priority: 4,
