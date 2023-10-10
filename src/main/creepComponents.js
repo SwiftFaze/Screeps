296e98e12d7ace6cbeceb76330fb887bf4ab29eb
Rob <RJJW.dev@gmail.com>
2023-10-10 18:11:46 +0200

    Add build new spawn

--- src/main/creepComponents.js
+++ src/main/creepComponents.js
@@ -88,7 +88,7 @@ const creepBuilds = {
         }, transporter: {
             priority: 2,
             quantity: 2,
-            body: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, WORK, MOVE, MOVE, MOVE, MOVE]
+            body: [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, WORK, MOVE, MOVE, MOVE, MOVE]
         }, upgrader: {
             priority: 3,
             quantity: 4,
