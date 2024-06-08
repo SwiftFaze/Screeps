145ce2a807a2d5b955cf84ccc3c38e2db0ddea97
Rob <RJJW.dev@gmail.com>
2024-06-08 20:04:56 +0200

    Added terminal transfer

--- src/main/terminal.js
+++ src/main/terminal.js
@@ -1,16 +1,10 @@
-const Structures = require('./structures')
 const Settings = require('./settings')
 
 function run() {
-    let terminal;
-
     for (var i in Game.rooms) {
         var room = Game.rooms[i];
-        terminal = Structures.getClosestEnergyStructure(creep, STRUCTURE_TERMINAL);
-
-        if (room.name !== Settings.TARGET_TERMINAL_TRANSFER_ROOM) {
-
-            if (terminal.store[RESOURCE_ENERGY] > Settings.TERMINAL_ENERGY_TRANSFER_THRESHOLD) {
+        if (room.name !== Settings.TARGET_TERMINAL_TRANSFER_ROOM && room.terminal !== undefined) {
+            if (room.terminal.store.getUsedCapacity(RESOURCE_ENERGY) > Settings.TERMINAL_ENERGY_TRANSFER_THRESHOLD) {
 
                 room.terminal.send(RESOURCE_ENERGY,
                     Settings.TERMINAL_ENERGY_TRANSFER_THRESHOLD,
